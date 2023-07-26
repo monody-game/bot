74940d63e2caf9d840b69f48eaa31f179af44da7
moon250 <mooneupho@gmail.com>
2023-07-26 20:41:33 +0200

    ref: remove dist files

--- dist/Utils/const.js
+++
@@ -1,6 +0,0 @@
-export var ServiceStatus;
-(function (ServiceStatus) {
-    ServiceStatus[ServiceStatus["Ok"] = 0] = "Ok";
-    ServiceStatus[ServiceStatus["Maintenance"] = 1] = "Maintenance";
-    ServiceStatus[ServiceStatus["Down"] = 2] = "Down";
-})(ServiceStatus || (ServiceStatus = {}));
--- dist/bootstrap.js
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-import { join } from "path";
-import { debug } from "@moon250/yalogger";
-debug("Loading .env");
-dotenv.config({
-    path: join(process.cwd(), ".env"),
-});
